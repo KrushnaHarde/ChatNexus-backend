a3f6d77b24e281f88831608cafb1bde4133a1509
Krushna <hardekrushna88@gmail.com>
2025-12-25 17:27:16 +0530

    first commit

---
+++ src/test/java/com/project/ChatNexus/ChatNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ChatNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
